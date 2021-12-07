c9a3e1d28d8f2e9823aca8b0d3887a76f34fe28b
Anuprita <ma3@dongbangchem.co.kr>
2021-12-08 08:32:24 +0900

    auth

---
+++ src/models/index.d.ts
@@ -0,0 +1,31 @@
+import { ModelInit, MutableModel, PersistentModelConstructor } from "@aws-amplify/datastore";
+
+
+
+
+
+type EmployeeMetaData = {
+  readOnlyFields: 'createdAt' | 'updatedAt';
+}
+
+export declare class Employee {
+  readonly id: string;
+  readonly empid?: number;
+  readonly empName?: string;
+  readonly position?: string;
+  readonly joindate?: string;
+  readonly totalworkdays?: number;
+  readonly avgannualsal?: number;
+  readonly avgsalthrmonth?: number;
+  readonly currentmonthpension?: number;
+  readonly Year2018?: number;
+  readonly Year2019?: number;
+  readonly Year2020?: number;
+  readonly Year2021Nov?: number;
+  readonly totalPension?: number;
+  readonly pensionHolder?: boolean;
+  readonly createdAt?: string;
+  readonly updatedAt?: string;
+  constructor(init: ModelInit<Employee, EmployeeMetaData>);
+  static copyOf(source: Employee, mutator: (draft: MutableModel<Employee, EmployeeMetaData>) => MutableModel<Employee, EmployeeMetaData> | void): Employee;
+}
